f7076f837d243ebb32c102be4690b08aa3da2cc8
purrrplelipton <immanueltoby@gmail.com>
2022-05-30 13:30:50 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,39 @@ import App from './App';
 
 import "./index.css";
 
-import axios from "axios";
 
-axios.get(
-  "http://localhost:4000/persons"
-).then(res => {
-  const persons = res.data;
-  ReactDOM.createRoot(
-    document.getElementById("root")
-  ).render(
-    <React.StrictMode>
-      <App persons={persons} />
-    </React.StrictMode>
-  );
-})
+const persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "123-456-789",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "Rhyan LeShawn",
+    "number": "+1 408-616-8483",
+    "id": 5
+  }
+]
 
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <App persons={persons} />
+  </React.StrictMode>
+);
--- src/services/Persons.js
+++ src/services/Persons.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = 'http://localhost:4000/persons'
+const baseURL = `http://localhost:3001/api/persons`
 
 function getAll() {
   const req = axios.get(baseURL)
